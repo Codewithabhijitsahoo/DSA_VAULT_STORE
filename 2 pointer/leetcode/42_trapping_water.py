64e80a44b4261c0c5fdd15c5aeb8cf1156f5e203
Abhijit_sahoo <abhijitsahoo945@gmail.com>
2026-06-10 22:12:21 +0530

    docs: update solution for trapping water (LeetCode)

---
+++ 2 pointer/leetcode/42_trapping_water.py
@@ -0,0 +1,22 @@
+height = [0,1,0,2,1,0,1,3,2,1,2,1]
+
+right_max=0
+left_max=0
+water=0
+left=0
+right=len(height)-1
+
+while left < right :
+
+  if height [left]<height [right]:
+    if height [left]>left_max:
+      left_max=height[left]
+    else:
+      water+=left_max-height[left]
+    left+=1
+  else:
+    if height [right]>right_max:
+      right_max=height[right]
+    else:
+      water+=right_max-height[right]
+    right-=1
