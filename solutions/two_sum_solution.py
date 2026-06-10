98532ba3d0f6ac0c96d552c42bed43bc159b3b19
Abhijit_sahoo <abhijitsahoo945@gmail.com>
2026-06-10 21:09:46 +0530

    feat: add two sum python solution

---
+++ solutions/two_sum_solution.py
@@ -0,0 +1,14 @@
+# Title: Two Sum
+# Difficulty: EASY
+# Platform: LeetCode
+# Link: https://leetcode.com/problems/two-sum/
+#
+
+class Solution:
+      def twoSum(self, nums: list[int], target: int) -> list[int]:
+        seen = {}
+        for i, num in enumerate(nums):
+          if target - num in seen:
+            return [seen[target - num], i]
+          seen[num] = i
+        return []
