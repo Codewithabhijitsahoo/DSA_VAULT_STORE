8ec99eca3ffa42285cc0ba2a8b922aa24488433c
Abhijit_sahoo <abhijitsahoo945@gmail.com>
2026-06-10 22:11:56 +0530

    docs: update solution for merged two sorted array (LeetCode)

---
+++ 2 pointer/leetcode/21_merged_two_sorted_array.py
@@ -0,0 +1,33 @@
+arr1 = [1,3,5]
+arr2 = [2,4,6]
+
+arr3 = [0] * (len(arr1) + len(arr2))
+
+i = 0
+j = 0
+k = 0
+
+while i < len(arr1) and j < len(arr2):
+
+    if arr1[i] < arr2[j]:
+        arr3[k] = arr1[i]
+        i += 1
+    else:
+        arr3[k] = arr2[j]
+        j += 1
+
+    k += 1
+
+# Remaining elements of arr1
+while i < len(arr1):
+    arr3[k] = arr1[i]
+    i += 1
+    k += 1
+
+# Remaining elements of arr2
+while j < len(arr2):
+    arr3[k] = arr2[j]
+    j += 1
+    k += 1
+
+print(arr3)
