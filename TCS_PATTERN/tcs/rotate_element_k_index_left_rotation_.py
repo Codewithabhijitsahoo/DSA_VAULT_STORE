e94147fcc7ceecf90671e2c0b005fec7df15c08b
Abhijit_sahoo <abhijitsahoo945@gmail.com>
2026-06-11 20:08:55 +0530

    docs: update solution for Rotate element k index ( left rotation ) (TCS)

---
+++ TCS_PATTERN/tcs/rotate_element_k_index_left_rotation_.py
@@ -0,0 +1,19 @@
+arr = [1, 2, 3, 4, 5, 6, 7]# 1 2  3 4 5 6 6
+                           # 1 2  3 4 5 5 6
+k = 2                    # 1 2  3 4 4 5 6
+n = len(arr)               # 1 2  3 3 4 5 6
+                           # 1 2  2 3 4 5 6
+count = 0                  # 6 1 2 3 4 5  1
+
+while count < k:
+    first_element = arr[0]
+
+    for i in range(1,n):
+        arr[i-1] = arr[i]
+
+    arr[n - 1] = first_element
+
+    count += 1
+
+print(arr)
+    
