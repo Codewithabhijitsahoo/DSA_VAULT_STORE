4c3196c51c435a927e7985c571f7c45d114345de
Abhijit_sahoo <abhijitsahoo945@gmail.com>
2026-06-14 14:08:12 +0530

    docs: add solution for 4 sum (TCS)

---
+++ TCS_PATTERN/tcs/4_sum.py
@@ -0,0 +1,40 @@
+nums = [1,0,-1,0,-2,2]
+nums.sort()
+
+target = 0
+
+for i in range(len(nums)-3):
+
+    if i > 0 and nums[i] == nums[i-1]:
+        continue
+
+    for j in range(i+1, len(nums)-2):
+
+        if j > i+1 and nums[j] == nums[j-1]:
+            continue
+
+        left = j+1
+        right = len(nums)-1
+
+        while left < right:
+
+            current_sum = nums[i] + nums[j] + nums[left] + nums[right]
+
+            if current_sum == target:
+
+                print(nums[i], nums[j], nums[left], nums[right])
+
+                left += 1
+                right -= 1
+
+                while left < right and nums[left] == nums[left-1]:
+                    left += 1
+
+                while left < right and nums[right] == nums[right+1]:
+                    right -= 1
+
+            elif current_sum < target:
+                left += 1
+
+            else:
+                right -= 1
