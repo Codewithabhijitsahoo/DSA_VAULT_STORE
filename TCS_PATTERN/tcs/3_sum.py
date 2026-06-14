4320b8dfafb08ea306299fa8919a95a6a857fe7b
Abhijit_sahoo <abhijitsahoo945@gmail.com>
2026-06-14 13:50:19 +0530

    docs: add solution for 3 sum (TCS)

---
+++ TCS_PATTERN/tcs/3_sum.py
@@ -0,0 +1,35 @@
+nums = [-1,0,1,2,-1,-4]
+nums.sort()
+
+target = 0
+
+for i in range(len(nums)-2):
+
+    if i > 0 and nums[i] == nums[i-1]:
+        continue
+
+    left = i + 1
+    right = len(nums)-1
+
+    while left < right:
+
+        current_sum = nums[i] + nums[left] + nums[right]
+
+        if current_sum == target:
+
+            print(nums[i], nums[left], nums[right])
+
+            left += 1
+            right -= 1
+
+            while left < right and nums[left] == nums[left-1]:
+                left += 1
+
+            while left < right and nums[right] == nums[right+1]:
+                right -= 1
+
+        elif current_sum < target:
+            left += 1
+
+        else:
+            right -= 1
